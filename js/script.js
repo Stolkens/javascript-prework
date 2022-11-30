94f0314a3e85d9ec0d8a0b07098d627c8e75480b
Stolkens <92957952+Stolkens@users.noreply.github.com>
2022-11-30 22:54:26 +0100

    Add camputerMove

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli twój ruch to '+ playerMove +', to wygrywasz!');
+let randomFraction = Math.random();
+let calculation = randomFraction *3 +1;
+let randomNumber = Math.floor(calculation);
+
+let computerMove = 'coś tam';
+
+if (randomNumber == 1){
+    computerMove = 'kamień';
+}
+else if ( randomNumber == 2){
+    computerMove = 'papier';
+}
+else {
+    computerMove = 'nożyce';
+}
+printMessage('Mój ruch to ' + computerMove);
+
