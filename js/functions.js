844cffe9304b777bcb390e9620542b926a289442
Stolkens <92957952+Stolkens@users.noreply.github.com>
2022-12-07 14:30:05 +0100

    Change variables

--- js/functions.js
+++ js/functions.js
@@ -1,92 +1,63 @@
-
-
-function getMoveName(moveId){
-	if (moveId == 1){
-		return 'kamień';
+const getMoveName = function(moveId) {
+	if (moveId == 1) {
+		return "kamień";
+	} else if (moveId == 2) {
+		return "papier";
+	} else if (moveId == 3) {
+		return "nożyce";
 	}
-	else if (moveId == 2){	
-		return 'papier';
-	}
-	else if (moveId == 3){
-		return 'nożyce';
-	}
-	printMessage ('Podałeś niewłaściwą wartość '+ moveId);
-	return 'nieznany ruch'
-	
+	printMessage("Podałeś niewłaściwą wartość " + moveId);
+	return "nieznany ruch";
 }
-function finalScore() {
-	if (resultcomp ==5 || resultplayer ==5) {
-		printFinalScore('Koniec gry!! Chcesz zagrać jeszcze raz?');
+const finalScore = function () {
+	if (resultcomp == 5 || resultplayer == 5) {
+		printFinalScore("Koniec gry!! Chcesz zagrać jeszcze raz?");
 		resultcomp = 0;
 		resultplayer = 0;
-		
 	}
-
 }
-function displayResult(computerMove, playerMove){
-	
-	printMessage('Ja zagrałem ' + computerMove  +', a ty '+ playerMove + ' więc ')
+const displayResult = function(computerMove, playerMove) {
+	printMessage(
+		"Ja zagrałem " + computerMove + ", a ty " + playerMove + " więc "
+	);
 	if (computerMove == playerMove) {
-		printMessage('Jest remis');
-		printResult('Twój wynik: '+ resultplayer + ' Mój wynik: '+ resultcomp);
-		
-	}
-	else if (computerMove == 'kamień' && playerMove == 'papier') {
-		printMessage('wygrałeś!');
+		printMessage("Jest remis");
+	} else if (computerMove == "kamień" && playerMove == "papier") {
+		printMessage("wygrałeś!");
 		resultplayer++;
-		printResult('Twój wynik: '+ resultplayer + ' Mój wynik: '+ resultcomp);
-		finalScore ();
-		
-		
-	}
-	else if (computerMove == 'papier' && playerMove == 'nożyce') {
-		printMessage('wygrałeś!');
+	} else if (computerMove == "papier" && playerMove == "nożyce") {
+		printMessage("wygrałeś!");
 		resultplayer++;
-		printResult('Twój wynik: '+ resultplayer + ' Mój wynik: '+ resultcomp);
-		finalScore ();
-		
-	}
-	else if (computerMove == 'nożyce' && playerMove == 'kamień') {
-		printMessage('wygrałeś!');
+	} else if (computerMove == "nożyce" && playerMove == "kamień") {
+		printMessage("wygrałeś!");
 		resultplayer++;
-		printResult('Twój wynik: '+ resultplayer + ' Mój wynik: '+ resultcomp);
-		finalScore ();
-		
-		
-	}
-	else {
-		printMessage('przegrałeś!');
+	} else {
+		printMessage("przegrałeś!");
 		resultcomp++;
-		printResult('Twój wynik: '+ resultplayer + ' Mój wynik: '+ resultcomp);
-		finalScore ();
-		
 	}
-
+	printResult("Twój wynik: " + resultplayer + " Mój wynik: " + resultcomp);
+	finalScore();
 }
 
-function printMessage(msg){
-	let div = document.createElement('div');
+const printMessage = function(msg) {
+	let div = document.createElement("div");
 	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+	document.getElementById("messages").appendChild(div);
 }
-function printResult(msg){
-	let div = document.createElement('div');
+const printResult = function(msg) {
+	let div = document.createElement("div");
 	div.innerHTML = msg;
-	document.getElementById('result').appendChild(div);
+	document.getElementById("result").appendChild(div);
 }
-function printFinalScore(msg){
-	let div = document.createElement('div');
+const printFinalScore = function(msg) {
+	let div = document.createElement("div");
 	div.innerHTML = msg;
-	document.getElementById('final-score').appendChild(div);
+	document.getElementById("final-score").appendChild(div);
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-function clearResults(){
-	document.getElementById('result').innerHTML = '';
-}
-function clearFinalScore () {
-	document.getElementById('final-score').innerHTML = '';
+const clearMessages = function() {
+	document.getElementById("messages").innerHTML = "";
+	document.getElementById("result").innerHTML = "";
+	document.getElementById("final-score").innerHTML = "";
 }
 
--- js/script.js
+++ js/script.js
@@ -1,48 +1,51 @@
-
-
-let resultplayer=0
-let resultcomp=0
+    
+{
+let resultplayer = 0
+let resultcomp = 0
+    
 
 function playGame (playerInput) {
-        
-    clearMessages ();
-    clearResults ();
-    clearFinalScore ();
-    let randomFraction = Math.random();
-    let calculation = randomFraction *3 +1;
-    let randomNumber = Math.floor(calculation);
-        
-    let computerMove = getMoveName(randomNumber);
     
-            // let playerInput = prompt ('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-        
-    console.log('Gracz wpisał ' + playerInput);
-    console.log('wylosowana liczba to '+ randomNumber);
-        
-    let playerMove = getMoveName(playerInput);
-        
-    displayResult(computerMove, playerMove);
-            
-            
+    
+    clearMessages ();
+    const randomFraction = Math.random();
+    const randomNumber = Math.floor(randomFraction *3+1);
+    
+    if(randomFraction < 0.75){  
+        playerMove = getMoveName (playerInput);
+        if (playerMove == 'kamień'){
+            computerMove = 'nożyce'
+        }
+        else if(playerMove == 'papier') {
+            computerMove = 'kamień'
+        }
+        else {
+            computerMove = 'papier'
+        }
+    }
+    else {
+        playerMove = getMoveName(playerInput);
+        computerMove = getMoveName(randomNumber);   
+    }     
+    displayResult(computerMove, playerMove);                  
 }
 
-
-function anonim1 (){
+function start1 (){
     playGame(1)
 }
 let button1 = document.getElementById('button1');
-button1.addEventListener('click', anonim1);
+button1.addEventListener('click', start1);
 
-function anonim2 (){
+function start2 (){
     playGame(2)
 }
 let button2 = document.getElementById('button2');
-button2.addEventListener('click', anonim2);
+button2.addEventListener('click', start2);
 
-function anonim3 (){
+function start3 (){
     playGame(3)
 }
 let button3 = document.getElementById('button3');
-button3.addEventListener('click', anonim3);
+button3.addEventListener('click', start3);
 
-
+}
